30d264ed97c5627a7ad118d5093a82998a7442bf
lavassa05 <vkathar91@gmail.com>
2022-01-03 09:41:24 +0530

    addidng eclipse project to github

---
+++ src/Pages/HomePage.java
@@ -0,0 +1,9 @@
+package Pages;
+
+public class HomePage {
+	
+	public static void main(String[] args) {
+		System.out.println("This is my hello Home page");
+	}
+
+}
---
+++ src/Pages/loginPage.java
@@ -0,0 +1,7 @@
+package Pages;
+
+public class loginPage {
+	public static void main(String[] args) {
+		System.out.println("This is my hello login page");
+	}
+}
